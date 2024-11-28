5a9fd31cbd0e3266aaed4fe3c732248ddf4e3a58
Gabriel Pires <gabrielpires2991@gmail.com>
2024-11-28 09:45:23 -0300

    add: swagger docs

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 from flask import Flask, jsonify, request 
 import utils as utils
+from flasgger import Swagger
 
 app = Flask(__name__)
+swagger = Swagger(app)
 
 @app.route('/hourly_consumption', methods=['GET'])
 def get_hourly_consumption():
+    """
+    Obter consumo horário
+    ---
+    responses:
+      200:
+        description: Consumo horário médio por eletrodomésticos e luzes
+      500:
+        description: Erro interno do servidor
+    """
     try:
         data = utils.spend_by_hour()
         return jsonify({'status': 'success', 'data': data})
@@ -13,6 +24,36 @@ def get_hourly_consumption():
 
 @app.route('/compare_months', methods=['GET'])
 def compare_months():
+    """
+    Comparar consumo entre dois meses
+    ---
+    parameters:
+      - name: month1
+        in: query
+        type: integer
+        required: true
+        description: Primeiro mês (1 a 12)
+      - name: year1
+        in: query
+        type: integer
+        required: true
+        description: Ano do primeiro mês
+      - name: month2
+        in: query
+        type: integer
+        required: true
+        description: Segundo mês (1 a 12)
+      - name: year2
+        in: query
+        type: integer
+        required: true
+        description: Ano do segundo mês
+    responses:
+      200:
+        description: Comparação de consumo entre os meses especificados
+      500:
+        description: Erro interno do servidor
+    """
     try:
         month1 = int(request.args.get('month1'))
         year1 = int(request.args.get('year1'))
@@ -26,6 +67,15 @@ def compare_months():
     
 @app.route('/calculate_appliance_contribution', methods=['GET'])
 def calculate_appliance_contribution():
+    """
+    Calcular contribuição de eletrodomésticos no consumo total
+    ---
+    responses:
+      200:
+        description: Contribuição de cada eletrodoméstico no consumo total
+      500:
+        description: Erro interno do servidor
+    """
     try:
         data = utils.calculate_appliance_contribution(utils.folder_path)
         return jsonify({'status': 'success', 'data': data})
@@ -35,6 +85,15 @@ def calculate_appliance_contribution():
 
 @app.route('/generate_tip', methods=['GET'])
 def generate_tip():
+    """
+    Gerar dicas para reduzir consumo
+    ---
+    responses:
+      200:
+        description: Dicas baseadas no consumo atual
+      500:
+        description: Erro interno do servidor
+    """
     try:
         data = utils.calculate_appliance_contribution(utils.folder_path)
         results = data['appliance_data']
@@ -51,6 +110,15 @@ def generate_tip():
 
 @app.route('/get_prediction', methods=['GET'])
 def get_prediction():
+    """
+    Gerar previsão de consumo
+    ---
+    responses:
+      200:
+        description: Previsão de consumo para os próximos dias
+      500:
+        description: Erro interno do servidor
+    """
     try:
         data = utils.generate_prediction()
         return jsonify({'status': 'success', 'data': data})
